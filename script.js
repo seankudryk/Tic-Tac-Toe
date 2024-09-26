759d75dfe84c400e99acb23b35ca154b7452d889
seankudryk <seankudryk@hotmail.ca>
2024-09-26 17:26:07 -0400

    created a gameboard object with a few encapsulated methods to get, set, and update the values of the board array

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+//gameboard object storing methods to get, set, and update the gameboard
 const gameboard = (function () {
-    const makeGameboard = () => {
-        
+    let board = [];
+
+    const getBoard = () => board;
+
+    const resetBoard = () => {
+        board = [[null, null, null], [null, null, null], [null, null, null]];
+        console.log(board);
     }
-    return {};
-})();
+
+    const updateBoard = () => {
+        console.log("This is a placeholder function");
+    }
+
+    return { getBoard, resetBoard, updateBoard };
+})();   
+
+
+//produce the default game board with all indexes set to null
+gameboard.resetBoard(); 
+
+const createPlayer = () => {
+
+}
