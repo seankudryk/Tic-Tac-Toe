f0c979e047bdeaf1c11944728ba9a25c4cd1c618
seankudryk <seankudryk@hotmail.ca>
2024-09-27 13:11:28 -0400

    created players object which stores playerOne and playerTwo along with their respective symbols - created the play round method of playGame which now references the players object to determine which player's turn it currently is, and then switches the turn to the next player once their turn has been taken

--- script.js
+++ script.js
@@ -16,10 +16,43 @@ const gameboard = (function () {
     return { getBoard, resetBoard, updateBoard };
 })();   
 
+const players = [
+    {
+        name: "playerOne",
+        symbol: "x"
+    },
+    {
+        name: "playerTwo",
+        symbol: "o"
+    }
+];
 
-//produce the default game board with all indexes set to null
-gameboard.resetBoard(); 
+//gameSetup function to createUser(s) and resetBoard
 
-const createPlayer = () => {
+const playGame = (function () {
+    //write function to createUser, taking a playerName input, and a playerSymbol input
 
-}
+    //reset board to ensure default game state is set
+    gameboard.resetBoard();
+
+    //set playerOne to go first
+    let playerTurn = players[0];
+
+    //initiate a player turn - we will call this method to "take turns" and actually play the game in console
+    const playRound = () => {
+        console.log(`it is ${playerTurn.name}'s turn`);
+        //what do we need to check?
+            //which player's turn is it?
+            //which space on the board does the player wish to place their symbol?
+            
+            
+
+            //once the user has placed their piece, change playerTurn to be NOT the current value of playerTurn
+            playerTurn = playerTurn === players[0] ? players[1] : players[0];
+    }
+        return { playRound };
+    }
+)();
+
+console.log(playGame.playRound());
+
