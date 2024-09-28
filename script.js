286f86436db43a6bd18c81d5520e9c6397a6d23b
seankudryk <seankudryk@hotmail.ca>
2024-09-28 12:56:19 -0400

    wrote a getCell method on the gameboard module which will be called by the playgame module to checkForWin

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const gameboard = (function () {
 
     const getBoard = () => {
         console.log(board);
-        board;
+        return board;
+    }
+
+    const getCell = (row, column) => {
+        return board[row][column];
     }
 
     const resetBoard = () => {
-        board = [[null, null, null], [null, null, null], [null, null, null]];
+        board = [["x", "x", "x"], [null, null, null], [null, null, null]];
     }
-
     const updateBoard = (row, column, symbol) => {
         if (board[row][column] !== null) {
             console.log("Not a valid selection");
@@ -18,7 +21,7 @@ const gameboard = (function () {
             return board[row][column] = symbol;
         }
     }
-    return { getBoard, resetBoard, updateBoard };
+    return { getBoard, getCell, resetBoard, updateBoard };
 })();   
 
 const players = [
@@ -34,12 +37,18 @@ const players = [
 
 const playGame = (function () {
     //write function to createUser, taking a playerName input, and a playerSymbol input
-
-    //reset board to ensure default game state is set
-    gameboard.resetBoard();
-
-    //set playerOne to go first
+    
     let playerTurn = players[0];
+    
+    //reset board to ensure default game state is set
+    const resetGame = () => {
+        gameboard.resetBoard();
+        playerTurn = players[0];
+    }
+
+    const checkForWin = () => {
+        console.log(gameboard.getCell(1, 2));
+    }
 
     //initiate a player turn - we will call this method to "take turns" and actually play the game in console
     const playRound = (row, column) => {
@@ -47,23 +56,24 @@ const playGame = (function () {
             console.log("Please enter both a row and column value");
             return;
         } else {
-            console.log(`It is ${playerTurn.name}'s turn`);
             //which space on the board does the player wish to place their symbol?
             gameboard.updateBoard(row, column, playerTurn.symbol);
+            console.log(`${playerTurn.name} chose Row ${row + 1}, Column ${column + 1} for ${playerTurn.symbol}`)
+            gameboard.getBoard();
                 
             //call checkForWin function which contains the win conditions for the game
                 //if checkForWin, then declare winner and prompt user to play again
     
             //once the user has placed their piece, change playerTurn to be NOT the current value of playerTurn
             playerTurn = playerTurn === players[0] ? players[1] : players[0];
+            console.log(`It is ${playerTurn.name}'s turn`);
         }
-        return { playRound };
     }
+    return { resetGame, checkForWin, playRound };
 })();
 
-gameboard.getBoard();
-// playGame.playRound(1, 2);
+
 // playGame.playRound(0, 1);
 // playGame.playRound(0, 2);
-// gameboard.getBoard();
+playGame.checkForWin();
 
